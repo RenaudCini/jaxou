5492ef13810a9d1fb1b6c3ade300094248dc066a
RenaudCini <43877413+RenaudCini@users.noreply.github.com>
2019-01-13 10:31:44 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+
+$('#Planning_lien').arctext({radius: 100 ,dir: -1});
+$('#Planning_lien_2').arctext({radius: 100 });
+$('#Information_lien').arctext({radius: 100 });
+$('#Information_lien_2').arctext({radius: 100,dir:-1 });
